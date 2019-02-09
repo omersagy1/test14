175954579fbc9b4dfe2ba11524c72fcff9642bd0
Omer Sagy <omersagyemails@gmail.com>
2019-02-09 12:40:10 -0500

    sample stateless component.

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,21 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Tile />
+        <Tile extra={"tile2"}/>
       </div>
     );
   }
 }
+
+const Tile = (props: {extra?: string}) => {
+  let display = props.extra || "tile"
+  return <div className="tile">{display}</div>;
+};
 
 export default App;
